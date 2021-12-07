139c1f6041b27ea51a3f97ef7385d74cfedfb266
Vitaliy Novak <almuert@yandex.ru>
2021-12-08 02:15:45 +0500

    Add new service for prepared data

--- registrar/src/app.go
+++ registrar/src/app.go
@@ -12,28 +12,36 @@ import (
 
 func main() {
 
+	brokerAddr := getEnv("KAFKA_HOST", "localhost")
+
 	brokerPort, err := strconv.Atoi(getEnv("KAFKA_PORT", "9092"))
-
 	if err != nil {
 		log.Fatalln(err)
 	}
+	topic := getEnv("KAFKA_TOPIC", "tocrm")
 
-	wPort, err := strconv.Atoi(getEnv("SERVICE_PORT", "15643"))
-
-	if err != nil {
-		log.Fatalln(err)
-	}
-
-	brokerAddr := getEnv("KAFKA_HOST", "127.0.0.1")
-	topic := getEnv("KAFKA_TOPIC", "change")
 	partition, err := strconv.Atoi(getEnv("KAFKA_TOPIC_PARTITION", "0"))
 
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	messageKey := getEnv("KAFKA_MESSAGE_KEY", "")
+
+	wPort, err := strconv.Atoi(getEnv("SERVICE_PORT", "8080"))
 
 	if err != nil {
 		log.Fatalln(err)
 	}
-	
-	s := registrar.NewServer(wPort, brokerAddr, brokerPort, topic, partition)
+
+	method := getEnv("SERVICE_METHOD", "")
+
+	checkInput := true
+	if v := getEnv("SERVICE_CHECK_INPUT", "0"); v == "1" {
+		checkInput = false
+	}
+
+	s := registrar.NewServer(wPort, brokerAddr, brokerPort, topic, partition, method, checkInput, messageKey)
 
 	//new chanel and exec subscription for handing
 	done := make(chan os.Signal, 1)
